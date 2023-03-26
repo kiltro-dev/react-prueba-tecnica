5e7bf5e1d0c47a4e35dffccaacf513d6e5a2aa19
Ignacio <55958716+dev-ignacio-h@users.noreply.github.com>
2023-03-26 18:18:54 -0300

    chore: test done

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { defineConfig, devices } = require('@playwright/test');
+import { defineConfig, devices } from '@playwright/test'
 
 /**
  * Read environment variables from file.
@@ -10,7 +10,7 @@ const { defineConfig, devices } = require('@playwright/test');
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
-module.exports = defineConfig({
+export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -28,25 +28,25 @@ module.exports = defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on-first-retry'
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'] }
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: { ...devices['Desktop Firefox'] }
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+      use: { ...devices['Desktop Safari'] }
+    }
 
     /* Test against mobile viewports. */
     // {
@@ -67,7 +67,7 @@ module.exports = defineConfig({
     //   name: 'Google Chrome',
     //   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ],
+  ]
 
   /* Run your local dev server before starting the tests */
   // webServer: {
@@ -75,5 +75,4 @@ module.exports = defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-});
-
+})
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,19 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test'
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const LOCALHOST_URL = 'http://localhost:5173/'
+const CAT_PREFIX_IMG_URL = 'https://cataas.com/'
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test('app shows random fact and image', async ({ page }) => {
+  await page.goto(LOCALHOST_URL)
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  const text = page.getByRole('paragraph')
+  const image = page.getByRole('img')
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  const textContent = await text.textContent()
+  const imageSrc = await image.getAttribute('src')
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
+  console.log({ textContent, imageSrc })
+  expect(textContent?.length).toBeGreaterThan(0)
+  expect(imageSrc?.startsWith(CAT_PREFIX_IMG_URL)).toBeTruthy()
+})
